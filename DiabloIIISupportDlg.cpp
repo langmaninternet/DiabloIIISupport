2805f79f8d7cf1b3ad0b6198599b7842e9437e17
Bùi Tấn Quang <langmaninternet@gmail.com>
2023-04-27 20:44:56 +0700

    Update

--- DiabloIIISupportDlg.cpp
+++ DiabloIIISupportDlg.cpp
@@ -1146,9 +1146,9 @@ void CDiabloIIISupportDlg::OnTimer(UINT_PTR nIdEvent)
 					//	if (flagOnCtrl9) Sleep(craftDelayTimeInMs + (rand() % 5));
 					//	
 					//	
-					//	if (flagOnCtrl9) SetD3Mouse(xSalvageButton, ySalvageButton);
-					//	if (flagOnCtrl9) SendD3LeftMouseClick();
-					//	if (flagOnCtrl9) Sleep(craftDelayTimeInMs + (rand() % 5));
+					if (flagOnCtrl9) SetD3Mouse(xSalvageButton, ySalvageButton);
+					if (flagOnCtrl9) SendD3LeftMouseClick();
+					if (flagOnCtrl9) Sleep(craftDelayTimeInMs + (rand() % 5));
 
 
 
