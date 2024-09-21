a14f72bab7693069dec812431431c4cf19bb511a
Bùi Tấn Quang <langmaninternet@gmail.com>
2024-09-21 17:39:24 +0700

    Update Tal Rasha.c

--- RosScript/Tal Rasha.c
+++ RosScript/Tal Rasha.c
@@ -764,23 +764,23 @@ Primary()>40
 
 
 
-//----------------------------------------------------------------------------------------------------------------------------------------
-//  #10.2 Teleport - Move - On [Wait for CoE Cycle|Infinite Casting] 
-// 	Force Recast
-//	Move
-//	1000ms
-//
-//  Note: On [Wait for CoE Cycle|Infinite Casting] 
-//
-BuffTimeLeft(0, Pages_Buff_Infinite_Casting) > 4000 | 
-(((Rune(Wizard_Meteor)==0&(BuffTimeLeft(5,P2_ItemPassive_Unique_Ring_038)>0|BuffTimeLeft(1,P2_ItemPassive_Unique_Ring_038)>0))
-|(Rune(Wizard_Meteor)==1&(BuffTimeLeft(5,P2_ItemPassive_Unique_Ring_038)>0|BuffTimeLeft(1,P2_ItemPassive_Unique_Ring_038)>0))
-|(Rune(Wizard_Meteor)==2&(BuffTimeLeft(3,P2_ItemPassive_Unique_Ring_038)>0|BuffTimeLeft(5,P2_ItemPassive_Unique_Ring_038)>0))
-|(Rune(Wizard_Meteor)==3&(BuffTimeLeft(2,P2_ItemPassive_Unique_Ring_038)>0|BuffTimeLeft(3,P2_ItemPassive_Unique_Ring_038)>0))
-|(Rune(Wizard_Meteor)==4&(BuffTimeLeft(1,P2_ItemPassive_Unique_Ring_038)>0|BuffTimeLeft(2,P2_ItemPassive_Unique_Ring_038)>0)))
-&
-(BuffTimeLeft(0,Wizard_Familiar)>1000|BuffTimeLeft(0,Wizard_MagicWeapon)>1000|BuffTimeLeft(3,Wizard_StormArmor)>1000|IsBuffActive(0, Pages_Buff_Run_Speed_Knockback_Cast)))
-//\\----------------------------------------------------------------------------------------------------------------------------------------////
+//		//----------------------------------------------------------------------------------------------------------------------------------------
+//		//  #10.2 Teleport - Move - On [Wait for CoE Cycle|Infinite Casting] 
+//		// 	Force Recast
+//		//	Move
+//		//	1000ms
+//		//
+//		//  Note: On [Wait for CoE Cycle|Infinite Casting] 
+//		//
+//		BuffTimeLeft(0, Pages_Buff_Infinite_Casting) > 4000 | 
+//		(((Rune(Wizard_Meteor)==0&(BuffTimeLeft(5,P2_ItemPassive_Unique_Ring_038)>0|BuffTimeLeft(1,P2_ItemPassive_Unique_Ring_038)>0))
+//		|(Rune(Wizard_Meteor)==1&(BuffTimeLeft(5,P2_ItemPassive_Unique_Ring_038)>0|BuffTimeLeft(1,P2_ItemPassive_Unique_Ring_038)>0))
+//		|(Rune(Wizard_Meteor)==2&(BuffTimeLeft(3,P2_ItemPassive_Unique_Ring_038)>0|BuffTimeLeft(5,P2_ItemPassive_Unique_Ring_038)>0))
+//		|(Rune(Wizard_Meteor)==3&(BuffTimeLeft(2,P2_ItemPassive_Unique_Ring_038)>0|BuffTimeLeft(3,P2_ItemPassive_Unique_Ring_038)>0))
+//		|(Rune(Wizard_Meteor)==4&(BuffTimeLeft(1,P2_ItemPassive_Unique_Ring_038)>0|BuffTimeLeft(2,P2_ItemPassive_Unique_Ring_038)>0)))
+//		&
+//		(BuffTimeLeft(0,Wizard_Familiar)>1000|BuffTimeLeft(0,Wizard_MagicWeapon)>1000|BuffTimeLeft(3,Wizard_StormArmor)>1000|IsBuffActive(0, Pages_Buff_Run_Speed_Knockback_Cast)))
+//		//\\----------------------------------------------------------------------------------------------------------------------------------------////
 
 
 
