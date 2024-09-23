d172fa185dd8f19c3a8fedb242c8ac923f2dab87
Bùi Tấn Quang <langmaninternet@gmail.com>
2024-09-23 08:08:19 +0700

    Update Hungering Arrow Build with Gears of Dreadlands.c

--- RosScript/Hungering Arrow Build with Gears of Dreadlands.c
+++ RosScript/Hungering Arrow Build with Gears of Dreadlands.c
@@ -1,97 +1,70 @@
 
 
+// #1 Vengeance
+// Buff
+// 1000ms
+// Force Recast
 
-//----------------------------------------------------------------------------------------------------------------------------------------
-//			Hungering Arrow - Buff - Momentum|Wraps of Clarity
-//
-//		Stand still: true
-//		Force recast: true
+
+// #2 Shadow Power
+// Buff
+// 50ms
+Secondary()>15&Health()<90%
+
+
+// #3 Smoke Screen
+// Buff
+// 50ms
+
+
+// #4 Fan of Knives
+// Attack+Buff
+// 50ms
+//		Attack Distance: 30
 //		Distance Min: 0
-//		Distance Max: 40
-//		Calculation min param: -10
+//		Distance Max: 30
+//		Calculation min param: 0
 //		Calculation max param: 10
-//		Attack limit: 0
-//		Elite weight: -1
-//		Minion weight: 0
-//		Big guy weight: -1
-//		Goblin weight: -1
-//		Normal monster weight: 0
+//		Attack limit: 1 
 //
-(
-(IsBuffActive(0,P69_ItemPassive_Unique_Ring_010)&(BuffTimeLeft(10,P69_ItemPassive_Unique_Ring_010)<2000|BuffStackCount(10, P69_ItemPassive_Unique_Ring_010)<17))
-|(IsBuffActive(0,P3_ItemPassive_Unique_Ring_038)&BuffTimeLeft(1,P3_ItemPassive_Unique_Ring_038)<1000)
-)
-
-
-//\\----------------------------------------------------------------------------------------------------------------------------------------////
-
-
-
+//		Elite weight: 10
+//		Minion weight: 1
+//		Normal monster weight: 1
+//		Big guy weight: 10
+//		Goblin weight: 10
+//
+//
 
 
 //----------------------------------------------------------------------------------------------------------------------------------------
-//			Hungering Arrow - Attack - Momentum|Wraps of Clarity
-//
+//		#5 Hungering Arrow - Buff - Momentum|Wraps of Clarity
+// 50ms
 //		Stand still: true
 //		Force recast: true
+//		Attack Distance: 40
 //		Distance Min: 0
 //		Distance Max: 40
 //		Calculation min param: 0
 //		Calculation max param: 10
-//		Attack limit: 1
-//		Elite weight: 2
-//		Minion weight: 0
-//		Big guy weight: 2
-//		Goblin weight: 2
-//		Normal monster weight: 0
+//		Attack limit: 1 
 //
-(
+//		Elite weight: 10
+//		Minion weight: 1
+//		Normal monster weight: 1
+//		Big guy weight: 10
+//		Goblin weight: 10
+//
 (IsBuffActive(0,P69_ItemPassive_Unique_Ring_010)&(BuffTimeLeft(10,P69_ItemPassive_Unique_Ring_010)<2000|BuffStackCount(10, P69_ItemPassive_Unique_Ring_010)<17))
 |(IsBuffActive(0,P3_ItemPassive_Unique_Ring_038)&BuffTimeLeft(1,P3_ItemPassive_Unique_Ring_038)<1000)
-)
-//\\----------------------------------------------------------------------------------------------------------------------------------------////
-
-
-
-
-
-
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------
-//	Hungering Arrow - Attack - Proc [Gear of Dreadlands - 4 pieces]
-//	Hungering Arrow - Buff - Proc [Gear of Dreadlands - 4 pieces]
-
-IsBuffActive(0,P69_ItemPassive_Unique_Ring_011)
-&(!IsBuffActive(4,P69_ItemPassive_Unique_Ring_010))
+|(IsBuffActive(0,P69_ItemPassive_Unique_Ring_011)&(!IsBuffActive(4,P69_ItemPassive_Unique_Ring_010)))
+|(IsBuffActive(0,P69_ItemPassive_Unique_Ring_010)&BuffTimeLeft(10,P69_ItemPassive_Unique_Ring_010)<2000)
+|(IsBuffActive(0,P69_ItemPassive_Unique_Ring_010)&BuffStackCount(10, P69_ItemPassive_Unique_Ring_010)<17&(BuffTimeLeft(0,X1_DemonHunter_Vengeance)>1000|BuffTimeLeft(1,DemonHunter_ShadowPower)>1000|IsBuffActive(0,DemonHunter_SmokeScreen)))
+|(IsBuffActive(0,ItemPassive_Unique_Ring_735_x1)&BuffTimeLeft(1,ItemPassive_Unique_Ring_735_x1)<2000)
 
 //\\----------------------------------------------------------------------------------------------------------------------------------------////
 
 
 
-//----------------------------------------------------------------------------------------------------------------------------------------
-//	Hungering Arrow - Attack - Maintain [Gear of Dreadlands - Momentum]
-//	Hungering Arrow - Buff - Maintain [Gear of Dreadlands - Momentum]
-
-IsBuffActive(0,P69_ItemPassive_Unique_Ring_010)
-&BuffTimeLeft(10,P69_ItemPassive_Unique_Ring_010)<2000
-
-//\\----------------------------------------------------------------------------------------------------------------------------------------////
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------
-//	Hungering Arrow - Attack - Increase [Gear of Dreadlands - Momentum]
-//	Hungering Arrow - Buff - Increase [Gear of Dreadlands - Momentum]
-
-IsBuffActive(0,P69_ItemPassive_Unique_Ring_010)
-&BuffStackCount(10, P69_ItemPassive_Unique_Ring_010)<17
-&(BuffTimeLeft(0,X1_DemonHunter_Vengeance)>1000|BuffTimeLeft(1,DemonHunter_ShadowPower)>1000|IsBuffActive(0,DemonHunter_SmokeScreen))
-
-//\\----------------------------------------------------------------------------------------------------------------------------------------////
-
-
-
-IsBuffActive(0,DemonHunter_SmokeScreen)
 
 
 
@@ -101,13 +74,6 @@ IsBuffActive(0,DemonHunter_SmokeScreen)
 
 
 
-//----------------------------------------------------------------------------------------------------------------------------------------
-//	Hungering Arrow - Proc [Focus]
-
-IsBuffActive(0,ItemPassive_Unique_Ring_735_x1)
-&BuffTimeLeft(1,ItemPassive_Unique_Ring_735_x1)<2000
-
-//\\----------------------------------------------------------------------------------------------------------------------------------------////
 
 
 
