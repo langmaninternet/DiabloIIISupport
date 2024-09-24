2ec37e74a501f5a640dfd482bb143282a12766e9
Bùi Tấn Quang <langmaninternet@gmail.com>
2024-09-24 23:48:02 +0700

    Update

--- RosScript/Hungering Arrow Build with Gears of Dreadlands.c
+++ RosScript/Hungering Arrow Build with Gears of Dreadlands.c
@@ -62,7 +62,7 @@ Secondary()>15&Health()<90%
 |(IsBuffActive(0,P69_ItemPassive_Unique_Ring_010)&BuffTimeLeft(10,P69_ItemPassive_Unique_Ring_010)<2000)
 |(IsBuffActive(0,P69_ItemPassive_Unique_Ring_010)&BuffStackCount(10, P69_ItemPassive_Unique_Ring_010)<17&(BuffTimeLeft(0,X1_DemonHunter_Vengeance)>1000|BuffTimeLeft(1,DemonHunter_ShadowPower)>1000|IsBuffActive(0,DemonHunter_SmokeScreen)))
 |(IsBuffActive(0,ItemPassive_Unique_Ring_735_x1)&BuffTimeLeft(1,ItemPassive_Unique_Ring_735_x1)<2000)
-
+|Primary()<20
 //\\----------------------------------------------------------------------------------------------------------------------------------------////
 
 
@@ -91,6 +91,22 @@ Secondary()>15&Health()<90%
 //			Min Resouce level 15
 //
 // 50ms
+
+
+//	Attack + Density + Buff
+//	50ms
+//		Attack Distance: 60
+//		Distance Min: 0
+//		Distance Max: 60
+//		Calculation min param: 0
+//		Calculation max param: 30
+//		Attack limit: 15, 
+//
+//		Elite weight: 15
+//		Minion weight: 10
+//		Normal monster weight:5
+//		Big guy weight: 15
+//		Goblin weight: 15
 
 
 
