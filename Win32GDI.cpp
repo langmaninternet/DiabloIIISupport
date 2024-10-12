9d50aca2f8621c37849cec82a560e0b6b1a66402
Bùi Tấn Quang <langmaninternet@gmail.com>
2024-10-12 18:18:15 +0700

    Add

--- Win32GDI.cpp
+++ Win32GDI.cpp
@@ -117,11 +117,11 @@ void		QuangBTDumpScreen(void)
 	//w32gdi.DumpSkill04();
 
 	//w32gdi.DumpRollOption01();
-	//w32gdi.DumpRollOption01Ex();
+	w32gdi.DumpRollOption01Ex();
 	//w32gdi.DumpRollOption02();
 	//w32gdi.DumpRollOption02Ex();
 	//w32gdi.DumpRollOption03();
-	w32gdi.DumpRollOption03Ex();
+	//w32gdi.DumpRollOption03Ex();
 
 	//w32gdi.DumpRectangle(208, 776, 319, 786);
 
