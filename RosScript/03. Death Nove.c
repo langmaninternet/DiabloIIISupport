6c7d285127d89b844282d48ebc459d58fcddaa26
Bùi Tấn Quang <langmaninternet@gmail.com>
2024-12-08 10:49:36 +0700

    Update 03. Death Nove.c

--- RosScript/03. Death Nove.c
+++ RosScript/03. Death Nove.c
@@ -2,31 +2,8 @@
 // #1. Simulacrum
 // Buff
 // 100ms
-
-
-
-
-
-//#2 Blood Rush
-//	Attack + Density
-//	5000ms
-//		Attack Distance: 60
-//		Distance Min: 0
-//		Distance Max: 60
-//		Calculation min param: 0
-//		Calculation max param: 30
-//		Attack limit: 1 
 //
-//		Elite weight: 10
-//		Minion weight: 3
-//		Normal monster weight: 0
-//		Big guy weight: 10
-//		Goblin weight: 10
-//
-//
-BuffStackCount(0,P6_Necro_BoneArmor)<5
-
-//#3 Bone Armor
+//#2 Bone Armor
 //	Density + Buff
 //	3000ms
 //		Attack Distance: 30
@@ -43,11 +20,8 @@ BuffStackCount(0,P6_Necro_BoneArmor)<5
 //		Goblin weight: 10
 //
 //
-BuffStackCount(0,P6_Necro_BoneArmor)<10
-|BuffTimeLeft(0,P6_Necro_BoneArmor)<1
 
-
-//#4 Siphon Blood
+//#3 Siphon Blood
 // Do not use in fast mode
 // Use to break item
 // 50ms
@@ -68,4 +42,27 @@ BuffStackCount(0,P6_Necro_BoneArmor)<10
 //		Goblin weight: 10
 //
 //
+
+
+
+//#4 Blood Rush
+//	Attack + Density
+//	3000ms
+//		Attack Distance: 90
+//		Distance Min: 0
+//		Distance Max: 90
+//		Calculation min param: 0
+//		Calculation max param: 30
+//		Attack limit: 1 
+//
+//		Elite weight: 10
+//		Minion weight: 3
+//		Normal monster weight: 0
+//		Big guy weight: 10
+//		Goblin weight: 10
+//
+//
+BuffStackCount(0,P6_Necro_BoneArmor)<5
+
+
 
